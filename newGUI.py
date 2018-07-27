31848d00e525a4fc7b6aaf68d4ef59ac42341b8a
Punsara-N <37718963+Punsara-N@users.noreply.github.com>
2018-07-27 15:48:56 +0100

    Finally got sizers to work well
    Still issue with aligning static text vertically in boxer...

--- newGUI.py
+++ newGUI.py
@@ -1,43 +1,40 @@
 import wx
 
-
-'''
-frame = wx.Frame(None, title="Hello World")
-frame.Show()
-'''
-
 class MainWindow(wx.Frame):
     
     def __init__(self, *args, **kw):
+        
         super(MainWindow, self).__init__(*args, **kw)
         self.SetTitle("Hello World!")
-        self.Show()
-        
-        self.panel1 = self._Panel((0,0),(200,200),(100, 100, 100))
-        self.panel2 = self._Panel((200,0),(200,200),(250, 250, 250))
+
+        self.panel1 = self._Panel((100, 100, 100))
+        self.panel2 = self._Panel((250, 250, 250))
+        self.panel3 = self._Panel((175, 175, 175))
         
         self.panelSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.panelSizer.Add(self.panel1,1,wx.ALIGN_CENTER)
-        self.panelSizer.Add(self.panel2,1,wx.ALIGN_CENTER)
-        self.SetSizer(self.panelSizer)
+        self.panelSizer.Add(self.panel1, proportion = 2, flag = wx.EXPAND)
+        self.panelSizer.Add(self.panel2, proportion = 3, flag = wx.EXPAND|wx.ALL, border = 30)
+        self.panelSizer.Add(self.panel3, proportion = 2, flag = wx.EXPAND)
+        self.SetSizerAndFit(self.panelSizer)
         
-    def _Panel(self, pos, size, colour):
-        self.panel = wx.Panel(self)
-        self.panel.SetPosition(pos)
-        self.panel.SetSize(size)
-        self.panel.SetBackgroundColour(colour)
+        self.Show()
         
-        self.textStatic = wx.StaticText(self.panel, label='Text:', pos=(10,10))
-        self.textBox    = wx.TextCtrl(self.panel, pos=(10,40))
+    def _Panel(self, colour):
         
-        space = 20
-        self.rowSizer = wx.BoxSizer(wx.HORIZONTAL)
-        #self.rowSizer.AddSpacer(space)
-        self.rowSizer.Add(self.textStatic,1,wx.ALIGN_CENTER)
-        #self.rowSizer.AddSpacer(space)
-        self.rowSizer.Add(self.textBox,1,wx.ALIGN_CENTER)
-        #self.rowSizer.AddSpacer(space)
-        self.panel.SetSizer(self.rowSizer)
+        panel = wx.Panel(self)
+        panel.SetBackgroundColour(colour)
+        
+        textStatic = wx.StaticText(panel, label='Text:', pos=(10,10))
+        textBox    = wx.TextCtrl(panel, pos=(10,40))
+        button     = wx.Button(panel, label='An example button')
+        
+        rowSizer = wx.BoxSizer(wx.VERTICAL)
+        rowSizer.Add(textStatic, proportion = 1, flag = wx.ALIGN_CENTER|wx.EXPAND|wx.ALL, border = 20)
+        rowSizer.Add(textBox,    proportion = 2, flag = wx.ALIGN_CENTER|wx.EXPAND|wx.ALL, border = 20)
+        rowSizer.Add(button,     proportion = 1, flag = wx.ALIGN_CENTER|wx.EXPAND|wx.ALL, border = 20)
+        panel.SetSizerAndFit(rowSizer)
+        
+        return panel
 
 
 def main():
