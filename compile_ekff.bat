e8343c1dfcf45e1f625e0a42659093b45796f7dc
Punsara-N <37718963+Punsara-N@users.noreply.github.com>
2018-08-15 11:36:50 +0100

    Continuation

---
+++ compile_ekff.bat
@@ -0,0 +1,1 @@
+g++ -O2 -shared -fPIC -Ie:\k\programs\Python27\include EKFF.cpp EKFF_wrap.cpp -LE:\k\programs\Python27\libs -lboost_python-mgw46-mt-s-1_46_1 -lpython27  -o ekff.pyd
--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-@start /D "C:\Users\PD Banneheka\Documents\GitHub\Manoeuvre-rig-and-load-cell-GUI" /B /REALTIME /AFFINITY F0 python newAPCGUI.py
+@start /D "C:\Users\PD Banneheka\Documents\GitHub\Manoeuvre-rig-and-load-cell-GUI" /B /REALTIME /AFFINITY F0 python AccessPointCenter.py
+cmd /k
